3868739a36dc3e56cda55328710bbb404e2054f1
Luke Bray <lukebray@hotmail.co.uk>
2019-06-12 22:42:03 +0100

    lift formulas

--- weightLifter.py
+++ weightLifter.py
@@ -1,29 +1,40 @@
 import random
 
 class WeightLifter:
-    def __init__(self, name, strength, skill, luck):
+    def __init__(self, name):
         self.name = name
-        self.strength = strength
-        self.skill = skill
-        self.luck = luck
-
+        self.bodyweight = round(random.uniform(50, 150), 2)
+        
     def calculateLifts(self):
-        self.snatch = self.strength * self.skill
-        self.clean_and_jerk = (self.strength * self.skill) * 1.3
-        return(self.snatch, self.clean_and_jerk)
-            
-competitor_amount = 5
-competitors = {}
-for x in range(competitor_amount):
-    skill = random.randint(1,11)
-    strength = random.randint(1,11)
-    luck = 1
+        self.snatch_max = round((1.067 * self.bodyweight) + 34.920, 0)
+        self.clean_and_jerk_max = round((1.28 * self.bodyweight) + 40.63, 0)
+        return(self.snatch_max, self.clean_and_jerk_max)
 
-    lifter = WeightLifter("Lifter " + str(x), strength, skill, luck)
-    lifter.calculateLifts()
-    competitors[lifter.name] = lifter.snatch
 
-winner = max(competitors, key=competitors.get)
-print("The winner of the snatch is " + winner + " with an " + str(competitors[winner]) + "kg lift!")
+class Attempt:
+    def __init__(self, weight):
+        self.weight = weight
+
+    def pass_or_fail(self):
+        factor = round(random.uniform(0,1), 2)
+        self.status = False
+        if factor > 0.5:
+            self.status = True
+        else:
+            self.status = False
+        return(self.status)
+
+
+
+w1 = WeightLifter("Luke") 
+w1.calculateLifts()
+print(w1.name)
+print("{}{}{}".format("Bodyweight: ", int(w1.bodyweight), "kg"))
+print("{}{}{}".format("Snatch: ", int(w1.snatch_max), "kg"))
+print("{}{}{}".format("Clean and Jerk: ", int(w1.clean_and_jerk_max), "kg"))
+
+a1 = Attempt(50)
+print(a1.pass_or_fail())
+
 
 
